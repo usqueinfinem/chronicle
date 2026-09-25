a7cb3850babadc2d354f16a146a74146a06b2831
usqueinfinem <arvind.singh.pec@gmail.com>
2026-09-25 13:59:20 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: '/ishikas-magical-chronicle/',
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
